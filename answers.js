803d483ef8386e438868f59cec713b8ed359d39d
anan17-meet <anan17@meet.mit.edu||>
2016-11-27 20:04:26 +0200

    first commit

--- answers.js
+++ answers.js
@@ -1,11 +1,100 @@
+
 ///PLEASE ADD YOUR SOLUTION CODE TO PART 1 HERE
 
 ///1. VARIABLES
 
+var temperature = 98.6
+temeprature = temeprature + 5 
+temeprature = temeprature * 3
+temeprature = temeprature - 20
+
+alert(temeprature)
+
 ///2. DATA STRUCTURES
 
+var Days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday" , "Thursday", "Friday", "Saturday"]
+
+Days_of_the_Week[0] = "domingo";
+Days_of_the_Week[1] = "lunes";
+Days_of_the_Week[2] = "martes";
+Days_of_the_Week[3] = "miercoles";
+Days_of_the_Week[4] = "Jueves";
+Days_of_the_Week[5] = "viernes";
+Days_of_the_Week[6] = "sabado";
+
+val iceCream = {"vanila": '$1.99', "chocolate": '$2.49', "straberry": '$2.25'}
+
+iceCream["chocolate"] = '$0.99'
 ///3. FUNCTIONS & CONDITIONAL STATEMENTS
 
+var DoIHaveToGoToSchoolToday = {"Sunday":"yes", "Monday":"yes", "Tuesday":"yes", "Wednesday":"yes", "Thursday":"yes", "Friday":"no", "Saturday":"no"};
+
+
+function DoIHaveToGoToSchoolToday(day_of_the_week){
+	var DoIHaveToGoToSchoolToday = {"Sunday":"yes", "Monday":"yes", "Tuesday":"yes", "Wednesday":"yes", "Thursday":"yes", "Friday":"no", "Saturday":"no"};
+	var answer = DoIHaveToGoToSchoolToday[day_of_the_week]
+	if (answer == "yes") {
+		alert("cool")
+	}
+}
+DoIHaveToGoToSchoolToday("Monday")
+ 
+
+
+
+
+
+
+
+
+///PLEASE ADD YOUR SOLUTION CODE TO PART 1 HERE
+
+///1. VARIABLES
+
+var temeprature = 98.6;
+temeprature = temeprature + 5 ;
+temeprature = temeprature * 3;
+temeprature = temeprature - 20;
+
+//alert(temeprature);
+
+///2. DATA STRUCTURES
+
+var Days_of_the_Week = ["Sunday", "Monday", "Tuesday", "Wednesday" , "Thursday", "Friday", "Saturday"];
+
+Days_of_the_Week[0] = "domingo";
+Days_of_the_Week[1] = "lunes";
+Days_of_the_Week[2] = "martes";
+Days_of_the_Week[3] = "miercoles";
+Days_of_the_Week[4] = "Jueves";
+Days_of_the_Week[5] = "viernes";
+Days_of_the_Week[6] = "sabado";
+
+var iceCream = {"vanila": '$1.99', "chocolate": '$2.49', "straberry": '$2.25'};
+
+iceCream["chocolate"] = '$0.99';
+
+//alert(iceCream["chocolate"]);
+///3. FUNCTIONS & CONDITIONAL STATEMENTS
+function DoIHaveToGoToSchoolToday(day_of_the_week){
+	var DoIHaveToGoToSchoolToday = {"Sunday":"yes", "Monday":"yes", "Tuesday":"yes", "Wednesday":"yes", "Thursday":"yes", "Friday":"no", "Saturday":"no"};
+	var answer = DoIHaveToGoToSchoolToday[day_of_the_week]
+	if (answer == "yes") {
+	alert("you need 2 go");
+}
+else {
+	alert("you don't need 2 go")
+}
+}
+DoIHaveToGoToSchoolToday("Friday");
 ///4.  LOOPS
+
+
+
+
+
+
+
+
 
 
